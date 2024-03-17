6d9aa8be9e836291bbd1ce4294369278852339c3
Bikash Wagle <waglelogan@Bikashs-MacBook-Air.local>
2024-03-17 21:14:54 +1100

    first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl" >
+        HomePage
+      </h1>
+      <link href='/about' className="text-2xl">about Page</link>
+    </div>
+  );
+};
+
+export default HomePage;
